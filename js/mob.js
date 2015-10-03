6d3c2e5ee18cbb66587d2cd835c53a7807d40d65
Colin <Colin.Watts@tufts.edu>
2015-10-03 14:44:15 -0400

    a bit of work on mobs

--- js/mob.js
+++ js/mob.js
@@ -1,11 +1,25 @@
 //Base code to govern mob behavior 
 
-var Mob = function() {
+var Mob = function(game, x, y) {
+	Phaser.Sprite.call(this, game, x, y, 'cyan');
 	this.game = game;
 	this.farmX = x;
 	this.farmY = y;
-	this.key = 'white';
+	this.key = 'cyan';
+	game.add.existing(this);
 }
 
 Mob.prototype = Object.create(Phaser.Sprite.prototype);
 Mob.prototype.constructor = Mob;
+Mob.prototype.moveSpeed = 0;
+Mob.prototype.attackSpeed = 0;
+Mob.prototype.attackRange = 0;
+Mob.prototype.sightRange = 0;
+Mob.prototype.health = 0;
+Mob.prototype.damage = 0;
+
+Mob.prototype.tick = function(farm, time) {
+	this.move();
+	this.render();
+
+}

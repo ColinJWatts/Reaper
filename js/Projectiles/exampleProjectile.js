bfde7ba0765e14e8dd31b12dc6f0f1e10b164e11
Colin <Colin.Watts@tufts.edu>
2015-10-11 15:06:17 -0400

    Example projectile now works properly

--- js/Projectiles/exampleProjectile.js
+++ js/Projectiles/exampleProjectile.js
@@ -2,20 +2,26 @@ exampleProjectile.prototype = Object.create(Proj.prototype);
 
 exampleProjectile.prototype.constructor = exampleProjectile;
 
-exampleProjectile.prototype.moveSpeed = 20;
+exampleProjectile.prototype.moveSpeed = 100;
 exampleProjectile.prototype.lifetime = 2;
 exampleProjectile.prototype.damage = 4;
 
+exampleProjectile.prototype.size = 0.5;
+
 function exampleProjectile(game, x, y, angle){
 	Phaser.Sprite.call(this, game, x, y, this.key);
-	this.angle = angle;
+	this.angle = 360*angle/(2*Math.PI);
+
+	this.scale.set(this.size, this.size);
+
 	this.anchor.set(0.5, 0.5);
 	game.physics.arcade.enable(this);
 	projectiles.add(this);
-	this.scale.set(this.size, this.size);
+	
 
-	this.body.velocity.x = this.moveSpeed*Math.sin(this.angle);
-	this.body.velocity.y = this.moveSpeed*Math.cos(this.angle);
+	this.body.velocity.x = this.moveSpeed*Math.cos(2*Math.PI*this.angle/360);
+	this.body.velocity.y = this.moveSpeed*Math.sin(2*Math.PI*this.angle/360);
+
 }
 
 exampleProjectile.prototype.move = function() {
